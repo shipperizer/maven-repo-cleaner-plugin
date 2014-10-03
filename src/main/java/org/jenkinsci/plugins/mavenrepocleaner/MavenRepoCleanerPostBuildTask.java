11d70be6807c2426977a01c0d83c7ef9681f911e
Alessandro Cabbia <acabbia@lp01787.openbet>
2014-10-03 10:38:47 +0100

    clean executor repos

--- src/main/java/org/jenkinsci/plugins/mavenrepocleaner/MavenRepoCleanerPostBuildTask.java
+++ src/main/java/org/jenkinsci/plugins/mavenrepocleaner/MavenRepoCleanerPostBuildTask.java
@@ -4,28 +4,30 @@ import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.maven.AbstractMavenProject;
-import hudson.maven.MavenModuleSet;
-import hudson.maven.MavenModuleSetBuild;
+import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.BuildListener;
 import hudson.model.FreeStyleProject;
 import hudson.remoting.VirtualChannel;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Publisher;
 import hudson.tasks.Recorder;
-import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Collection;
+
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * @author: <a hef="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
-public class MavenRepoCleanerPostBuildTask extends Recorder {
-
+public class MavenRepoCleanerPostBuildTask extends Recorder implements Serializable{
+	
+	private static String maven_repo = "maven-repositories"; 
+	
     @DataBoundConstructor
     public MavenRepoCleanerPostBuildTask() {
 
@@ -41,17 +43,29 @@ public class MavenRepoCleanerPostBuildTask extends Recorder {
                     return new RepositoryCleaner(started).clean(repository);
                 }
             };
-        Collection<String> removed = build.getWorkspace().child(".repository").act(cleanup);
-        if (removed.size() > 0) {
-            listener.getLogger().println( removed.size() + " unused artifacts removed from private maven repository" );
+        Collection<String> removed = null;
+        String ex_no = null;
+        try{    
+        	ex_no = build.getEnvironment().get("EXECUTOR_NUMBER");
+        	removed = build.getWorkspace().getParent().getParent().child(maven_repo).child(ex_no).act(cleanup);
+        	listener.getLogger().println(build.getWorkspace().getParent().getParent().child(maven_repo).child(ex_no).getRemote());
+        	if (removed.size() > 0) {
+                listener.getLogger().println( removed.size() + " unused artifacts removed from private maven repository" );
+            }
+        	else {
+        		listener.getLogger().println( "Nothing to delete" );
+        	}
+        } catch(Exception e){
+        	e.printStackTrace(listener.error("Failed to delete"));
+        	listener.getLogger().println("CRAP CRAP CRAP");
         }
         return true;
     }
 
-    public BuildStepMonitor getRequiredMonitorService() {
+    public BuildStepMonitor getRequiredMonitorService() {  
         return BuildStepMonitor.NONE;
     }
-
+    
     @Extension
     public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
--- src/main/java/org/jenkinsci/plugins/mavenrepocleaner/RepositoryCleaner.java
+++ src/main/java/org/jenkinsci/plugins/mavenrepocleaner/RepositoryCleaner.java
@@ -1,48 +1,50 @@
 package org.jenkinsci.plugins.mavenrepocleaner;
 
+import hudson.os.PosixAPI;
+
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import hudson.os.PosixAPI;
 import org.apache.commons.io.DirectoryWalker;
-import org.apache.maven.artifact.repository.metadata.Metadata;
-import org.apache.maven.artifact.repository.metadata.Snapshot;
 import org.apache.maven.index.artifact.Gav;
 import org.apache.maven.index.artifact.M2GavCalculator;
 import org.jruby.ext.posix.FileStat;
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Hello world!
  *
  */
-public class RepositoryCleaner extends DirectoryWalker
+public class RepositoryCleaner extends DirectoryWalker implements Serializable
 {
     private M2GavCalculator gavCalculator = new M2GavCalculator();
-    private long olderThan;
+    private long timeVar;
     private String root;
-
+    private static final Logger LOGGER = Logger.getLogger(RepositoryCleaner.class.getName()); 
+    
     public RepositoryCleaner(long timestamp) {
-        this.olderThan = timestamp / 1000;
+    	int days = 7;
+        this.timeVar = (timestamp/ 1000)-(days*24*60*60);
+        
     }
 
     public Collection<String> clean(File repository) throws IOException {
-        this.root = repository.getAbsolutePath();
+    	LOGGER.log(Level.WARNING, "CLEAN" + repository.toString() );
+    	this.root = repository.getAbsolutePath();
         Collection<String> result = new ArrayList<String>();
         walk(repository, result);
         return result;
     }
 
     protected final void handleDirectoryStart(File directory, int depth, Collection results) throws IOException {
-
+    	LOGGER.log(Level.WARNING, "HANDLE DIRECTORY START");
         for (File file : directory.listFiles()) {
             if (file.isDirectory()) continue;
             String fileName = file.getName();
@@ -65,18 +67,21 @@ public class RepositoryCleaner extends DirectoryWalker
 
     }
 
-    private void olderThan(File file, Gav artifact, Collection results) {
+    private void olderThan(File file, Gav artifact, Collection<String> results) {
+    	LOGGER.log(Level.WARNING, "OLDER THAN");
         FileStat fs = PosixAPI.get().lstat(file.getPath());
         long lastAccessTime = fs.atime();
-        if (lastAccessTime < olderThan) {
+        
+        if (lastAccessTime < timeVar) {
             // This artifact hasn't been accessed during build
             clean(file, artifact, results);
         }
     }
 
-    private void clean(File file, Gav artifact, Collection results) {
-        File directory = file.getParentFile();
+    private void clean(File file, Gav artifact, Collection<String> results) {
+    	File directory = file.getParentFile();
         String fineName = gavCalculator.calculateArtifactName(artifact);
+        LOGGER.log(Level.WARNING, "CLEAN "+ fineName);
         new File(directory, fineName + ".md5").delete();
         new File(directory, fineName + ".sha1").delete();
         file.delete();
